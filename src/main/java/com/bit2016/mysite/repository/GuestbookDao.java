46b9ffa9c55c7374ce0f34d1b30f5d99e3381979
sunyoung <bit-user1@bit-user>
2016-11-07 17:04:06 +0900

    1107 guestbook mybatis 완성

--- src/main/java/com/bit2016/mysite/repository/GuestbookDao.java
+++ src/main/java/com/bit2016/mysite/repository/GuestbookDao.java
@@ -1,9 +1,5 @@
 package com.bit2016.mysite.repository;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
@@ -16,74 +12,16 @@ import com.bit2016.mysite.vo.GuestbookVo;
 public class GuestbookDao {
 	@Autowired
 	private SqlSession sqlSession;
-	
-	private Connection getConnection() throws SQLException {
-		Connection conn = null;
 
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			String url = "jdbc:oracle:thin:@localhost:1521:xe";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
-
-		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 오류 :" + e);
-		}
-
-		return conn;
-	}
 	public void delete(GuestbookVo vo) {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		try {
-			conn = getConnection();
-			String sql = "delete from guestbook where no = ? and password = ?";
-
-			pstmt = conn.prepareStatement(sql);
-
-			pstmt.setLong(1, vo.getNo());
-			pstmt.setString(2, vo.getPassword());
-
-			pstmt.executeUpdate();
-
-		} catch (SQLException e) {
-			System.out.println("error : " + e);
-		} finally {
-			try {
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				System.out.println("error : " + e);
-			}
-		}
+		
+		sqlSession.delete("guestbook.delete", vo);
+		
 	}
 
 	public void insert(GuestbookVo vo) {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		try {
-			conn = getConnection();
-			String sql = "insert into GUESTBOOK VALUES(guestbook_seq.nextval, ?, ?, ?, sysdate)";
-
-			pstmt = conn.prepareStatement(sql);
-
-			pstmt.setString(1, vo.getName());
-			pstmt.setString(2, vo.getContent());
-			pstmt.setString(3, vo.getPassword());
-			
-			pstmt.executeUpdate();
-
-		} catch (SQLException e) {
-			System.out.println("error : " + e);
-		} finally {
-			try {
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				System.out.println("error : " + e);
-			}
-		}
+		
+		sqlSession.insert("guestbook.insert",vo);
 	}
 
 	public List<GuestbookVo> getList(){
